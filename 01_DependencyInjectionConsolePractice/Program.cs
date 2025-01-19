2632770233647947c730f68124a708f0843c5bd7
burakmahim <burakmahim718@gmail.com>
2025-01-19 18:47:38 +0300

    Proje dosyası ekle.

---
+++ 01_DependencyInjectionConsolePractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using _01_DependencyInjectionConsolePractice;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Teacher instance'ı oluştur
+        ITeacher teacher = new Teacher("Burak", "Mahim");
+
+        // Teacher'ı ClassRoom'a inject et
+        ClassRoom classroom = new ClassRoom(teacher);
+
+
+        //Boş Constructor ile oluşturup propertyleri sonradan verme alternatifi
+
+        //ITeacher teacher1 = new Teacher
+        //{
+        //    FirstName = "Ahmet",
+        //    LastName = "Yılmaz"
+        //};
+
+
+        Console.Write("Öğretmen: ");
+
+        // Bilgileri yazdır
+        Console.WriteLine(classroom.GetTeacherInfo());
+
+        Console.ReadKey();
+    }
+}
